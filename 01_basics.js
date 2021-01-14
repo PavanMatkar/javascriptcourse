ac0a2cf1304c7d0fadbffc20f95fd4069453ee6f
Pavan S Matkar <pavanmatkar123@gmail.com>
2021-01-14 17:24:31 +0530

    variables discussion

---
+++ 01_basics.js
@@ -0,0 +1,9 @@
+console.log("Welcome to Javascript");   //Welcome to Javascript
+console.log( 100+200 );     //300   (Addition)
+console.log( "100"+100 );   //100100    (Concatination)
+console.log( 100-"100" );   //0
+console.log( "10"*10 );     //100
+console.log( "10" / "10" );     //1
+console.log( 10 + +"10" );      //20
+console.log( +"100" + +"100" );     //200
+console.log( 0.1 + 0.2 );       //0.30000000000000004
